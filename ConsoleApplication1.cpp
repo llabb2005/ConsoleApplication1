84bf1906c8e99176bdf17a0f4ae8e31c056d373f
llabb2005 <4B3G0059@office.stust.edu.tw>
2024-10-14 23:10:38 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int i;
+int j;
+void printMultiplicationTable(int n) {
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			cout << i << "*" << j << "=" << i * j << "\t";
+		}
+		cout << endl;
+	}
+}
+	int main() {
+		int n = 1;
+		cout << "請輸入乘法表的大小:";
+		cin >> n;
+
+		printMultiplicationTable(n);
+
+		return 0;
+	}
